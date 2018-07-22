ee9ed4c81d5bae6282c01548f44bd4f761e0bd22
Carl Walsh <darthwalsh@gmail.com>
2018-07-22 08:48:31 -0700

    ToText script
    Script speach to text of AAC
    Convert to FLAC if needed

---
+++ ToText.ps1
@@ -0,0 +1,34 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $File
+)
+$script:ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+if (!(Test-Path $File)) {
+  throw "$File doesn't exist!"
+}
+if ([System.IO.Path]::GetExtension($File) -ne ".aac") {
+  throw "expected extension .AAC"
+}
+
+$flac = [System.IO.Path]::GetFileNameWithoutExtension($File) + ".flac"
+
+if (!(Test-Path $flac)) {
+  echo "Converting $File to $flac..."
+  ffmpeg -loglevel warning -i "$File" -ac 1 "$flac"
+  # gCloud doesn't support AAC, so use FLAC format
+  # Use one audio channel, as required by gCloud API
+} else { 
+  echo "$flac already exists"
+}
+
+$json = [System.IO.Path]::GetFileNameWithoutExtension($File) + ".json"
+
+if (!(Test-Path $json)) {
+  gcloud ml speech recognize-long-running --language-code en-US --format json "$flac" > $json
+} else { 
+  echo "$json already exists"
+}
+
+gc $json
